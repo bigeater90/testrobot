67a63c9eb838d4704ab659befffcbe9f042465c1
Ricain <jeanmercadier@gmail.com>
2013-12-07 21:59:24 +0100

    fin test map tools

--- test/MapToolsUnitTest.java
+++ test/MapToolsUnitTest.java
@@ -44,4 +44,76 @@ public class MapToolsUnitTest {
         Assert.assertEquals(0, position.getY());
     }
 
+
+    @Test
+    public void testNextBackwardPosition1() {
+        Coordinates ini = new Coordinates(0, 0);
+        Coordinates position = MapTools.nextBackwardPosition(ini, Direction.NORTH);
+        Assert.assertEquals(0, position.getX());
+        Assert.assertEquals(-1, position.getY());
+    }
+
+    @Test
+    public void testNextBackwardPosition2() {
+        Coordinates ini = new Coordinates(0, 0);
+        Coordinates position = MapTools.nextBackwardPosition(ini, Direction.SOUTH);
+        Assert.assertEquals(0, position.getX());
+        Assert.assertEquals(1, position.getY());
+    }
+
+    @Test
+    public void testNextBackwardPosition3() {
+        Coordinates ini = new Coordinates(0, 0);
+        Coordinates position = MapTools.nextBackwardPosition(ini, Direction.WEST);
+        Assert.assertEquals(1, position.getX());
+        Assert.assertEquals(0, position.getY());
+    }
+
+    @Test
+    public void testNextBackwardPosition4() {
+        Coordinates ini = new Coordinates(0, 0);
+        Coordinates position = MapTools.nextBackwardPosition(ini, Direction.EAST);
+        Assert.assertEquals(-1, position.getX());
+        Assert.assertEquals(0, position.getY());
+    }
+
+    @Test
+    public void counterclockwise1(){
+        Assert.assertEquals(Direction.WEST,MapTools.counterclockwise(Direction.NORTH));
+    }
+
+    @Test
+    public void counterclockwise2(){
+        Assert.assertEquals(Direction.SOUTH,MapTools.counterclockwise(Direction.WEST));
+    }
+
+    @Test
+    public void counterclockwise3(){
+        Assert.assertEquals(Direction.EAST,MapTools.counterclockwise(Direction.SOUTH));
+    }
+
+    @Test
+    public void counterclockwise4(){
+        Assert.assertEquals(Direction.NORTH,MapTools.counterclockwise(Direction.EAST));
+    }
+
+    @Test
+    public void clockwise1(){
+        Assert.assertEquals(Direction.EAST,MapTools.clockwise(Direction.NORTH));
+    }
+
+    @Test
+    public void clockwise2(){
+        Assert.assertEquals(Direction.SOUTH,MapTools.clockwise(Direction.EAST));
+    }
+
+    @Test
+    public void clockwise3(){
+        Assert.assertEquals(Direction.WEST,MapTools.clockwise(Direction.SOUTH));
+    }
+
+    @Test
+    public void clockwise4(){
+        Assert.assertEquals(Direction.NORTH,MapTools.clockwise(Direction.WEST));
+    }
 }
